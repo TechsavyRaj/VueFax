1e3b996c01d82e415b09aa088139064daba04740
Raj-Dev-Web <raj@zonvoir.com>
2026-01-22 22:29:59 +0530

    feat : VueFax app

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup>
+import src from '@/assets/logo_vue_300px.png'
+</script>
+
+<template>
+    <header>
+        <img :src alt="vue logo">
+        <h1>Hi, I'm <span class="highlight">Vue</span></h1>
+    </header>
+</template>
+
+<style scoped>
+img {
+    height: 150px;
+}
+
+h1 {
+    font-size: 2rem;
+    color: #34495e;
+    margin-top: 10px;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,61 @@
+<script setup>
+import {ref} from 'vue'
+const content = ref([
+  {
+    "title": "Lightweight",
+    "description": "I am incredibly small and fast! My core library is only around 30KB, so I won't slow you down."
+  },
+  {
+    "title": "Approachable",
+    "description": "Easy to learn and use, even for beginners. I have a gentle learning curve, clear documentation, and a supportive community."
+  },
+  {
+    "title": "Versatile",
+    "description": "I can handle everything from simple interactive elements to complex single-page applications. I'm great for small projects and large-scale applications alike."
+  },
+])
+</script>
+
+<template>
+  <main>
+      <section>
+          <h2>
+              I'm <span class="highlight">{{content[0].title}}</span>
+          </h2>
+          <p>{{content[0].description}}</p>
+      </section>
+      <section>
+          <h2>
+              I'm <span class="highlight">{{content[1].title}}</span>
+          </h2>
+          <p>{{content[1].description}}</p>
+      </section>
+      <section>
+          <h2>
+              I'm <span class="highlight">{{content[2].title}}</span>
+          </h2>
+          <p>{{content[2].description}}</p>
+      </section>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    margin-top: 15px;
+  }
+
+  section {
+    margin-top: 25px;
+  }
+
+  h2 {
+    font-size: 1.3rem;
+    color: #34495e;
+    margin-bottom: 3px;
+  }
+
+  p {
+    font-size: 0.9rem;
+    color: #555;
+  }
+</style>
